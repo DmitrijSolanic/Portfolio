662c7945b7f8dea670a85adb4372c414b7230df6
Dmitrij <dmitrij.solanic@gmail.com>
2024-02-28 10:43:31 +0300

    initial commit

---
+++ Missed_Opportunity_Calculator/README.md
@@ -0,0 +1,39 @@
+# Missed Opportunity Calculator
+
+### What is Dollar Cost Averaging?
+
+Dollar Cost Averaging (DCA) simulates effect of regular investments an asset without regard for price/market fluctuations or trying to time markets. DCA is a popular investment strategy that does not depend on your ability to time the markets and has been [mathematically](https://ofdollarsanddata.com/even-god-couldnt-beat-dollar-cost-averaging/) proven to yield better results over the long run than 'buying the dip' even if the the investor happens to be omniscient and able to perfectly time the market.
+
+
+### What does missed opportunity calculator do?
+
+Missed opportunity calculator lets you determine 4 parameters: **asset, frequency of investment, contribution and start date**. 
+
+Using your parameters it will calculate:
+- how much money you would have invested in an asset until present day
+- how much such a portfolio would be worth based on the latest prize
+- what your profit/loss would be on the investment.
+  
+A list of all dates when investments take place along with historical stock price, count of shares bought, running invested funds and running portfolio value is provided.
+Here are some examples of what using the output of the calculator might look like
+
+[/assets/images/](https://github.com/DmitrijSolanic/Missed_Opportunity_Calculator)
+https://github.com/DmitrijSolanic/Missed_Opportunity_Calculator/missed_opportunity_calculator_example1.JPG
+/Missed_Opportunity_Calculator/missed_opportunity_calculator_example2.JPG
+/Missed_Opportunity_Calculator/missed_opportunity_calculator_example3_viz.JPG
+
+
+### How to use the calculator?
+
+1. **Assets** can be *stocks, cryptocurrencies or commodities* as long as they have a ticker on Yahoo finance, which I am using as a data source for asset closing price at each trading day. You can check out trending ticker on [Yahoo](https://finance.yahoo.com/trending-tickers/).
+2. There are three **frequency** options to choose from: *weekly, monthly and yearly*. 
+In case of weekly, Mondays are used, in case of monthly, 1st day of month is used, in case of yearly, January 1st is used. 
+In cases when these are not trading days for a given asset, data from the closest following trading day is used. 
+3. **Contribution** is how much money you are investing into the asset in dollars on a given frequency. 
+4. **Date** of your first contribution is either start date if coincides with starting day of chosen frequency or the starting day of subsequent week/month/year.
+
+
+### Disclaimer
+
+Provided for informational/educational purposes only and should not be construed as investment advice . Past performance may not be not indicative of future results. Always consult your Investment Adviser before any decision. 
+
---
+++ Missed_Opportunity_Calculator/missed_opportunity_calculator.py
@@ -0,0 +1,145 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+
+# Seting display options for pandas
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.max_rows', None)     # Show all rows
+pd.set_option('display.width', None)  # Set the width to fit all columns
+
+# Welcome message
+print("\nWelcome to the missed opportunity calculator. \nFind out how much an asset of your choosing would be worth today if you started investing regularly at a point in the past.")
+
+# Gathering inputs
+ticker = input("Please input ticker of your preffered asset (as shown on Yahoo finance, e.g. TSLA for Tesla/ AAPL for Apple/ ^GSPC for S&P500/ GC=F for Gold etc.): ")
+frequency = input("Please input frequency of conributions (w for weekly/ m for monthly/ y for yearly): ")
+start_date = input("Please input a starting date in YYYY-MM-DD format: ")
+dca_amount_input = input("Please input the amount you wish you had been investing in dollars: ")
+
+# Converting to date format and subtracting 10 days
+start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
+new_date_obj = start_date_obj - timedelta(days=10)
+
+# Creating first Dataframe, default index is the date, so resetting it in order to be able to work with date as column
+asset = yf.Ticker(ticker)
+asset_history = asset.history(start=new_date_obj)
+df = pd.DataFrame(asset_history)
+df.reset_index(inplace=True)
+
+# Re-formatting date back and finding last trading day before start date
+df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+condition = df['Date'] < start_date
+trading_day_before_start_date = df.loc[condition, 'Date'].iloc[-1]
+
+# Re-creating Dataframewith predated date to be able to use lag later effectively.
+# If starting date is used directly as start parameter in following line, lagged_week_filled will not differ from week for any Monday etc.
+asset_history = asset.history(start=trading_day_before_start_date)
+df = pd.DataFrame(asset_history)
+df.reset_index(inplace=True)
+
+# Calculating how much stock could be bought on each day at close
+dca_amount = float(dca_amount_input)
+df['Stock_bought'] = dca_amount / df['Close']
+
+# Performing relevant date based transformations and calculations depending on chosen frequency
+if frequency == 'w':
+    # Creating a week column based on date column
+    df['week'] = df['Date'].dt.isocalendar().week
+    # Creating a lagged version of the week column
+    df['lagged_week'] = df['week'].shift(1)
+    # Filling in week value into the first line of lagged_week column which is by default NaN
+    df['lagged_week_filled'] = df['lagged_week'].fillna(df['week'])
+    # Checking whether day is first day of a new week
+    df['same_week'] = (df['week'] == df['lagged_week_filled']).astype(int)
+    # Filtering df for first day of every week
+    week_start_slice = df[df['same_week'] == 0]
+    # Creating a proper DF, so that I can reformat without errors
+    week_start_df = week_start_slice.copy()
+    # Reformatting for better visual clarity
+    week_start_df['Date'] = week_start_df['Date'].dt.strftime('%Y-%m-%d')
+    week_start_df['Closing price'] = week_start_df['Close'].round(2)
+    week_start_df['Stock_bought'] = week_start_df['Stock_bought'].round(2)
+    chosen_df = week_start_df
+    basis = 'weekly'
+elif frequency == 'm':
+    # Same process with month and year columns
+    df['month'] = df['Date'].dt.month
+    df['lagged_month'] = df['month'].shift(1)
+    df['lagged_month_filled'] = df['lagged_month'].fillna(df['month'])
+    df['same_month'] = (df['month'] == df['lagged_month_filled']).astype(int)
+    month_start_slice = df[df['same_month'] == 0]
+    month_start_df = month_start_slice.copy()
+    month_start_df['Date'] = month_start_df['Date'].dt.strftime('%Y-%m-%d')
+    month_start_df['Closing_price'] = month_start_df['Close'].round(2)
+    month_start_df['Stock_bought'] = month_start_df['Stock_bought'].round(2)
+    chosen_df = month_start_df
+    basis = 'monthly'
+else:
+    df['year'] = df['Date'].dt.year
+    df['lagged_year'] = df['year'].shift(1)
+    df['lagged_year_filled'] = df['lagged_year'].fillna(df['year'])
+    df['same_year'] = (df['year'] == df['lagged_year_filled']).astype(int)
+    year_start_slice = df[df['same_year'] == 0]
+    year_start_df = year_start_slice.copy()
+    year_start_df['Date'] = year_start_df['Date'].dt.strftime('%Y-%m-%d')
+    year_start_df['Closing_price'] = year_start_df['Close'].round(2)
+    year_start_df['Stock_bought'] = year_start_df['Stock_bought'].round(2)
+    chosen_df = year_start_df
+    basis = 'yearly'
+
+chosen_df.reset_index(inplace=True)
+chosen_df = chosen_df[['Date','Closing_price','Stock_bought']]
+chosen_df = chosen_df.copy()
+# Adding clumns
+chosen_df['running_stock_unit_total'] = chosen_df['Stock_bought'].cumsum()
+chosen_df['running_portfolio_value'] = round(chosen_df['running_stock_unit_total']  * chosen_df['Closing_price'])
+chosen_df['amount_invested'] = ((chosen_df.index + 1) * dca_amount).round()
+chosen_df['running_pl'] = round((chosen_df['running_portfolio_value'] / chosen_df['amount_invested'])*100)
+
+# Definitions prep for last_price_row and final message
+latest_trading_date = df['Date'].iloc[-1]
+latest_trading_date_reformatted = latest_trading_date.strftime('%Y-%m-%d')
+invested_funds = chosen_df['amount_invested'].iloc[-1]
+overall_portfolio = round(chosen_df['running_stock_unit_total'].iloc[-1] * df['Close'].iloc[-1])
+overall_shares = chosen_df['running_stock_unit_total'].iloc[-1]
+
+p_l =round(((overall_portfolio / invested_funds ) -1 )*100)
+if p_l < 0:
+    p_l_word = 'loss'
+else: p_l_word = 'profit'
+absolute_pl = abs(p_l)
+
+# Adding last row with latest price and appending the new row to the DataFrame
+latest_price_row = {'Date': latest_trading_date_reformatted ,
+                    'Closing_price': round(df['Close'].iloc[-1],2) ,
+                    'running_stock_unit_total': overall_shares ,
+                    'running_portfolio_value': overall_portfolio ,
+                    'amount_invested': invested_funds ,
+                    'running_pl':  round((chosen_df['running_stock_unit_total'].iloc[-1] * df['Close'].iloc[-1] / chosen_df['amount_invested'].iloc[-1])*100) }
+latest_price_row_df = pd.DataFrame([latest_price_row])
+chosen_df = pd.concat([chosen_df, latest_price_row_df], ignore_index=True)
+gains_losses = overall_portfolio - invested_funds
+
+# Converting each string date to a date object without changing the format for correct reading by matplotlib
+chosen_df['Date'] = chosen_df['Date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
+
+# Final dataset
+print("\n",chosen_df)
+
+# Final personalized message
+print("\nInvesting",dca_amount_input,"USD on a",basis,"basis in",ticker,"starting from",start_date,"till today means your total contribution would be",round(invested_funds),"USD."
+      "\nThese funds would have generated a",p_l_word,"of",round(abs(gains_losses)),"USD (a",p_l_word,"of",absolute_pl,"%) with position currently valued at",overall_portfolio,"USD (",round(overall_shares, 2),"shares/coins ).")
+if p_l < 0:
+    print("\nCongratulations on not following that idea through.")
+else:
+    print("\nDon't be sad. Remember the saying 'The best time to start investing was 10 years ago, the next best time is now.'")
+
+# Visualisation
+chosen_df.plot(x = 'Date', y= ['amount_invested', 'running_portfolio_value'], style=['o-', 'o-'])
+plt.legend(labels=['Amount Invested ($)', 'Portfolio Value ($)'], loc='upper left')
+plt.title(ticker + ' portfolio - the story of what could have been if you DCA-ed')
+fig = plt.gcf()
+fig.set_size_inches(13, 6)
+plt.grid(True)
+plt.show()
